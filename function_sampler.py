10d05037eda5ad89cd03ff4839d8831fcf80ba65
Brian Ho <bho@Brians-MacBook-Pro.local>
2015-09-01 20:40:31 -0400

    Initialized some files for the demo function approximator.

---
+++ function_sampler.py
@@ -0,0 +1,14 @@
+# This script uses Theano to implement a function sampler. The user can specify
+# a separate Python file that implements the 'func' function (also written in
+# Theano). This script will then sample n number of points and output them to
+# standard output.
+#
+# Brian Ho
+# bho6@jhu.edu
+# 9/1/15
+
+def main():
+    print "to be implemented"
+
+if __name__ == "__main__":
+    main()
